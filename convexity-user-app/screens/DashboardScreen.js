89c79684934a223c5b19c8c90a32ba2f296c2dd2
emmaodia <odia.emma@gmail.com>
2020-07-24 03:40:41 +0100

    CHORE: Created DashboardScreen.js file

---
+++ convexity-user-app/screens/DashboardScreen.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// import { SplashScreen } from 'expo';
+
+ const SplashScreen = props => {
+ 
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default SplashScreen;
