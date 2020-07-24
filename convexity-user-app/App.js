16f5da827450ea77628e3a45707b0ab3114def23
emmaodia <odia.emma@gmail.com>
2020-07-24 04:01:10 +0100

    CHORE: Removed commented code fromApp.js file.

--- convexity-user-app/App.js
+++ convexity-user-app/App.js
@@ -2,9 +2,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppLoading } from 'expo';
-import { NavigationContainer } from '@react-navigation/native';
 
-// import SplashScreen from './screens/SplashScreen'
 import AppNavigation from './navigation'
 
 export default function App() {
