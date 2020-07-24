37a5d72f24ec79f618ee5c3fa48fbf9d1e46cccd
emmaodia <odia.emma@gmail.com>
2020-07-24 03:42:34 +0100

    CHORE: Updated Dashboard in App Navigation.

--- convexity-user-app/navigation/index.js
+++ convexity-user-app/navigation/index.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from '../screens/SplashScreen';
 import Login from '../screens/LoginScreen';
 import SignUp from '../screens/SignUpScreen';
-import Dashboard from '../screens/Dashboard';
+import Dashboard from '../screens/DashboardScreen';
 import UserTerms from '../screens/UserTermsScreen';
 
 const Stack = createStackNavigator();
