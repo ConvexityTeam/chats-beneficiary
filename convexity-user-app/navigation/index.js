cd75951f7c18984cbc74fb28db8303a8b1c01c06
emmaodia <odia.emma@gmail.com>
2020-07-27 02:09:18 +0100

    CHORE: Updated navigation/index.js with menuDrawer implementation for Dashboard.

--- convexity-user-app/navigation/index.js
+++ convexity-user-app/navigation/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SplashScreen from '../screens/SplashScreen';
-import Login from '../screens/LoginScreen';
-import SignUp from '../screens/SignUpScreen';
-import Dashboard from '../screens/DashboardScreen';
-import UserTerms from '../screens/UserTermsScreen';
+import SplashScreen from '../screens/index/SplashScreen';
+import Login from '../screens/index/LoginScreen';
+import SignUp from '../screens/index/SignUpScreen';
+import Dashboard from '../screens/index/DashboardScreen';
+import UserTerms from '../screens/index/UserTermsScreen';
 
 const Stack = createStackNavigator();
+
+/*
+  Line 15 - 35 Sets the Drawer items nested in the Dashboard Component at line 44
+  You can read https://reactnavigation.org/docs/screen-options-resolution/#setting-parent-screen-options-based-on-child-navigators-state
+  to understand ho this is implemented.
+  */
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+
+function getHeaderTitle(route) {
+
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Dashboard';
+
+  switch (routeName) {
+    case 'Profile':
+      return 'Profile';
+    case 'Pay Bills':
+      return 'Pay Bills';
+    case 'Wallet':
+      return 'Wallet';
+    case 'Notifications':
+      return 'Notifications';
+    case 'Settings':
+      return 'Settings';
+    case 'Vendors':
+      return 'Vendor';
+  }
+}
+
 
 const AppNavigation = () => {
     return (
@@ -15,9 +43,11 @@ const AppNavigation = () => {
           <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name="Home" component={SplashScreen} />
             <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen name="Dashboard" component={Dashboard} options={({ route }) => ({
+    headerTitle: getHeaderTitle(route),
+  })} />
             <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="UserTerms" component={UserTerms} />
+            <Stack.Screen name="UserTerms" component={UserTerms} options={{ title: 'User Terms' }}/>
           </Stack.Navigator>
         </NavigationContainer>
       );
