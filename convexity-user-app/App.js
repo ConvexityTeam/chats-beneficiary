ac314e77f80ccc26bb8c4a07296ebe0e056c9d41
emmaodia <odia.emma@gmail.com>
2020-07-28 04:36:16 +0100

    Begin set up of RN Paper theme

--- convexity-user-app/App.js
+++ convexity-user-app/App.js
@@ -1,24 +1,29 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
 
 // import SplashScreen from './screens/SplashScreen'
 import AppNavigation from './navigation'
 
+import theme from './constants/PaperTheme'
+
 export default function App() {
  
   return (
+    <PaperProvider>
       <AppNavigation />
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 50,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   },
+// });
