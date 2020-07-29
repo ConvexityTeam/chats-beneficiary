6a601e47919fbb7a0aa4af55b064e0c10152c711
emmaodia <odia.emma@gmail.com>
2020-07-29 05:18:09 +0100

    Loaded App fonts in the State of the App on Launch

--- convexity-user-app/App.js
+++ convexity-user-app/App.js
@@ -1,16 +1,37 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
+import * as Font from 'expo-font';
 
 // import SplashScreen from './screens/SplashScreen'
 import AppNavigation from './navigation'
 
 import theme from './constants/PaperTheme'
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'inter-black': require('./assets/fonts/Inter-Black.ttf'),
+    'inter-bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'inter-regular': require('./assets/fonts/Inter-Regular.ttf')
+  })
+}
+
 export default function App() {
+
+  const  [fontLoaded, setFontLoaded ] = useState(false)
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    )
+  }
+
  
   return (
     <PaperProvider>
@@ -18,12 +39,3 @@ export default function App() {
     </PaperProvider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     paddingTop: 50,
-//     justifyContent: 'center',
-//     alignItems: 'center'
-//   },
-// });
