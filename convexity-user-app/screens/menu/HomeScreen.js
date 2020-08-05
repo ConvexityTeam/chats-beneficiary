746e558ba92d49befbb5f81eda8a46eac904c470
emmaodia <odia.emma@gmail.com>
2020-08-05 11:36:53 +0100

    CHORE: Updated Homescreen.

--- convexity-user-app/screens/menu/HomeScreen.js
+++ convexity-user-app/screens/menu/HomeScreen.js
@@ -1,45 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCreditCard, faMoneyBill,faChartBar, faCog, faExchangeAlt, faUniversity } from '@fortawesome/free-solid-svg-icons'
-import Colors from '../../constants/colors'
+import Colors from '../../constants/colors';
+import { TouchableRipple } from 'react-native-paper';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ( props) => {
 
     return (
       <View style={styles.container}>
         <View style={styles.header}>
         <Text style={{
-            fontSize: 35,
+            fontSize: 30,
             fontFamily: 'inter-bold'
             }}>Dashboard</Text>
         </View>
           <View style={styles.name}>
-            <Text style={{color: Colors.purple, fontFamily: 'inter-regular', lineHeight: 39, fontSize: 25, fontStyle: "normal"}}>Emmanuel Oaikhenan</Text>
+            <Text style={{color: Colors.purple, fontFamily: 'inter-regular', lineHeight: 24, fontSize: 18, fontStyle: "normal"}}>Emmanuel Oaikhenan</Text>
+            <Text style={{fontFamily: "inter-bold", fontSize: 14, lineHeight: 15}}>Balance: 15,000 NGNT</Text>
           </View>
 
           <View style={{flexDirection: "row", 
           justifyContent: "space-evenly"}}>
-            <View style={styles.card}>
-              <FontAwesomeIcon icon={ faExchangeAlt } style={{color: Colors.purple}} />
-              <Text style={styles.text}>Convert</Text>
-            </View>
-            <View style={styles.card}>
-              <FontAwesomeIcon icon={ faUniversity } style={{color: Colors.purple}} />
-              <Text style={styles.text}>Withdraw</Text>
-            </View>
-            <View style={styles.card}>
-              <FontAwesomeIcon icon={ faMoneyBill } style={{color: Colors.purple}} />
-              <Text style={styles.text}>Pay</Text>
-            </View>
+            <TouchableOpacity onPress={() => {
+                props.navigation.navigate('ConvertToken')
+            }} >
+              <View style={styles.card}>
+                <FontAwesomeIcon icon={ faExchangeAlt } style={{color: Colors.purple}} />
+                <Text style={styles.text}>Convert</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+                props.navigation.navigate('WithdrawToBank')
+            }} >
+              <View style={styles.card}>
+                <FontAwesomeIcon icon={ faUniversity } style={{color: Colors.purple}} />
+                <Text style={styles.text}>Withdraw</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {
+                props.navigation.navigate('Pay')
+            }} >
+              <View style={styles.card}>
+                <FontAwesomeIcon icon={ faMoneyBill } style={{color: Colors.purple}} />
+                <Text style={styles.text}>Pay</Text>
+              </View>
+            </TouchableOpacity>
           </View>
 
           <View style={{flexDirection: "row", justifyContent: "space-evenly"}}>
+
+          <TouchableOpacity onPress={() => {
+                props.navigation.navigate('Wallet')
+            }} >
             <View style={styles.card}>
             <FontAwesomeIcon icon={ faCreditCard } style={{color: Colors.purple}} />
-              <Text style={styles.text}>Wallet</Text>
+            
+              <Text style={styles.text} >Wallet</Text>
+             
             </View>
+            </TouchableOpacity>
+
             <View style={styles.card}>
             <FontAwesomeIcon icon={ faChartBar } style={{color: Colors.purple}} />
               <Text style={styles.text}>Analytics</Text>
@@ -50,9 +72,7 @@ const HomeScreen = ({ navigation }) => {
             </View>
           </View>
           <View style={{paddingTop: 100}}>
-            <View style={{flexDirection: "row", 
-            justifyContent: "space-between", alignItems: "center", 
-            padding: 10}}>
+            <View style={styles.name}>
               <Text style={{fontFamily: "inter-bold", fontSize: 22, lineHeight: 27, fontStyle: "normal"}}>Transactions</Text>
               <Text style={{fontFamily: "inter-regular", fontSize: 12, lineHeight: 15, fontStyle: "normal"}}>View All</Text>
             </View>
@@ -85,7 +105,10 @@ const styles = StyleSheet.create({
     padding: 10
   },
   name: {
-    padding: 10
+    padding: 10,
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    alignItems: "center", 
   },
   card: {
     margin: 3,
