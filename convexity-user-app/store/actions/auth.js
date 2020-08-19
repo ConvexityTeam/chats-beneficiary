608c163f99f271878dfab4609e86b8dce10b3e7a
emmaodia <odia.emma@gmail.com>
2020-08-19 03:22:56 +0100

    CHORE - Login Action and Error Handling for auth implemented.

--- convexity-user-app/store/actions/auth.js
+++ convexity-user-app/store/actions/auth.js
@@ -1,27 +1,72 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
-    return async dispatch => {
-        const response = await fetch(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwgIWUA56_htYICcVGRnHdxPNYAbPTzPI', 
-            {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                returnSecureToken: true
-            })
+  // console.log("email");
+  // console.log(email);
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAwgIWUA56_htYICcVGRnHdxPNYAbPTzPI',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true
         })
+      });
 
-        if(!response.ok) {
-            throw new Error('Something went wromg!')
+    if (!response.ok) {
+        const errorResData = await response.json();
+        const errorId = errorResData.error.message;
+        let message = 'Something went wrong!';
+        if (errorId === 'EMAIL_EXISTS') {
+          message = 'This email is already registered!';
         }
-
-        const resDate = response.json();
-        console.log(resData);  
-        dispatch({ type: SIGNUP})
+        throw new Error(message);
     }
-}
+    
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+    return async dispatch => {
+      const response = await fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAwgIWUA56_htYICcVGRnHdxPNYAbPTzPI',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true
+          })
+        }
+      );
+  
+      if (!response.ok) {
+        const errorResData = await response.json();
+        const errorId = errorResData.error.message;
+        let message = 'Something went wrong!';
+        if (errorId === 'EMAIL_NOT_FOUND') {
+          message = 'This user is not registered!';
+        } else if (errorId === 'INVALID_PASSWORD') {
+          message = 'This password is not valid!';
+        }
+        throw new Error(message);
+      }
+  
+      const resData = await response.json();
+      console.log(resData);
+      dispatch({ type: LOGIN });
+    };
+  };
+  
