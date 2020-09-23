d4b04f941caf04a79e51db4b2b9be1c7372d0399
emmaodia <odia.emma@gmail.com>
2020-09-23 13:45:17 +0100

    FIX: Fixed Drawer Navigation Icons.

--- convexity-user-app/navigation/menuDrawer.js
+++ convexity-user-app/navigation/menuDrawer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Button, SafeAreaView } from 'react-native';
+import { View, Text, Button, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
   createDrawerNavigator,
@@ -7,6 +8,10 @@ import {
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHome, faWallet, faChartBar, faCog, faQrcode, faUser, faSignOutAlt, faHeadset, faUniversity, faBell, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
 
 import { useDispatch } from 'react-redux';
 import * as authActions from '../store/actions/auth';
@@ -37,44 +42,69 @@ function LogOut({ navigation }) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button onPress={() => {
         dispatch(authActions.logout())
-        navigation.navigate('Auth')
+        navigation.navigate('Login')
       }} title="Log Out" />
       <Text>Are you sure?</Text>
     </View>
   );
 }
 
-// const CustomDrawerContent = props=> {
-//   return (
-//     <DrawerContentScrollView {...props}>
-//       <DrawerItemList {...props} />
-//       <DrawerItem
-//         label="Close drawer"
-//         onPress={() => props.navigation.closeDrawer()}
-//       />
-//       <DrawerItem
-//         label="Toggle drawer"
-//         onPress={() => props.navigation.toggleDrawer()}
-//       />
-//     </DrawerContentScrollView>
-//   );
-// }
+const CustomDrawerContent = props=> {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      {/* <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      /> */}
+      {/* <View style={styles.header}>
+        <FontAwesomeIcon icon={ faChartBar } style={{color: '#000'}} />
+        <View style={{paddingLeft: 5}}>
+          <DrawerItem
+            label="Toggle drawer"
+            onPress={() => props.navigation.toggleDrawer()}
+          />
+        </View>
+      </View> */}
+      <View style={styles.header}>
+        <FontAwesomeIcon icon={ faHeadset } style={{color: '#000'}} />
+        <View style={{paddingLeft: 10}}>
+          <DrawerItem
+            label="Contact Us"
+            onPress={() => props.navigation.navigate('Dashboard', { screen: 'Profile' })}
+          />
+        </View>
+      </View>
+      
+    </DrawerContentScrollView>
+  );
+}
 
 const Drawer = createDrawerNavigator();
 
 function MenuDrawer() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Dashboard" component={Home} options={{ title: 'Home' }} style={{color: '#fff'}}/>
-      <Drawer.Screen name="Profile" component={Profile} options={{ title: 'Profile' }}/>
-      <Drawer.Screen name="Wallet" component={Wallet} />
-      <Drawer.Screen name="QR Code" component={PayBills} />
-      <Drawer.Screen name="Settings" component={AppSettings} />
-      <Drawer.Screen name="Notifications" component={Notifications} />
-      <Drawer.Screen name="Vendors" component={Vendor} />
-      <Drawer.Screen name="Logout" component={LogOut} />
+    <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen name="Dashboard" component={Home} options={{ title: 'Home', drawerIcon: config => <FontAwesomeIcon icon={ faHome } style={{color: '#000'}} /> }} />
+      <Drawer.Screen name="Wallet" component={Wallet} options={{
+            drawerIcon: config => <FontAwesomeIcon icon={ faWallet } style={{color: '#000'}} /> }} />
+      <Drawer.Screen name="Generate/Scan QR Code" component={PayBills} options={{drawerIcon: config => <FontAwesomeIcon icon={ faQrcode } style={{color: '#000'}} />}} />
+      <Drawer.Screen name="Notifications" component={Notifications} options={{drawerIcon: config => <FontAwesomeIcon icon={ faBell } style={{color: '#000'}} />}} />
+      <Drawer.Screen name="Profile" component={Profile} options={{drawerIcon: config => <FontAwesomeIcon icon={ faUser } style={{color: '#000'}} />}} />
+      {/* <Drawer.Screen name="Vendors" component={Vendor} /> */}
+      <Drawer.Screen name="Settings" component={AppSettings} options={{drawerIcon: config => <FontAwesomeIcon icon={ faCog } style={{color: '#000'}} />}} />
+      <Drawer.Screen name="Logout" component={LogOut} options={{drawerIcon: config => <FontAwesomeIcon icon={ faSignOutAlt } style={{color: '#000'}} />}} /> 
     </Drawer.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    paddingLeft: 20,
+    // paddingRight: 10,
+    flexDirection: "row", 
+    alignItems: "center"
+  },
+})
 
 export default MenuDrawer;
