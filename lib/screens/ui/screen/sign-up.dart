6b1f2574475078b3f20950a4cadd9fb19f77641f
ae-allison <ansonervin@yahoo.com>
2021-03-26 04:26:49 -0500

    Finished reorganization

---
+++ lib/screens/ui/screen/sign-up.dart
@@ -0,0 +1,366 @@
+// import 'dart:io';
+
+// import 'package:CHATS/core/utils/otp_pin.dart';
+
+// import 'package:file_picker/file_picker.dart';
+
+// import 'package:CHATS/Chats%20Main/core/models/bene_user.dart';
+
+import 'package:CHATS/ChatsMain/core/models/beneficiary_user_model.dart';
+import 'package:CHATS/utils/otp_pin.dart';
+import 'package:CHATS/ChatsMain/ui/shared/BTN.dart';
+import 'package:CHATS/ChatsMain/ui/shared/TEXT.dart';
+import 'package:CHATS/ChatsMain/ui/shared/TF.dart';
+import 'package:CHATS/ChatsMain/ui/shared/ui_helper.dart';
+import 'package:CHATS/ChatsMain/ui/viewModels/base_view_model.dart';
+import 'package:CHATS/ChatsMain/ui/viewModels/sign_upVM.dart';
+import 'package:flutter/material.dart';
+// import 'package:pin_entry_text_field/pin_entry_text_field.dart';
+
+class SignUpView extends StatefulWidget {
+  @override
+  _SignUpViewState createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final smallH = size.height / 36;
+    final smallW = size.height / 46;
+    return BaseViewModel<SignUpVM>(
+      providerReady: (model) {},
+      builder: (context, provider, child) => WillPopScope(
+        // ignore: missing_return
+        onWillPop: () {
+          if (_formStage != 1) {
+            setState(() {
+              --_formStage;
+            });
+          } else
+            Navigator.pop(context);
+        },
+        child: Scaffold(
+          backgroundColor: Color.fromRGBO(250, 250, 250, 1),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                  top: smallH * 2,
+                  right: smallW,
+                  left: smallW,
+                  bottom: smallH * 3),
+              child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      child: Image.asset(
+                        'assets/logo.png',
+                        width: smallW * 10,
+                      ),
+                      margin: EdgeInsets.only(bottom: smallH * 1.2),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: smallH * 1.2),
+                    padding:
+                        EdgeInsets.only(right: smallW * 4, left: smallW * 4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Icon(Icons.check, color: Colors.white),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(style: BorderStyle.none),
+                              color: Constants.purple),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Divider(
+                              color: _formStage >= 2
+                                  ? Constants.purple
+                                  : Color.fromRGBO(196, 196, 196, 1),
+                              height: 2,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Icon(Icons.check, color: Colors.white),
+                          decoration: BoxDecoration(
+                            border: _formStage >= 2
+                                ? Border.all(style: BorderStyle.none)
+                                : Border.all(width: 2, color: Constants.purple),
+                            color: _formStage >= 2
+                                ? Constants.purple
+                                : Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  buildStage(smallH, provider)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget personInformation(double smallH) {
+    return Form(
+      key: myKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TEXT(
+            text: 'Personal Information',
+            fontFamily: 'Gilroy-bold',
+            fontSize: smallH * 1.5,
+            edgeInset: EdgeInsets.only(bottom: smallH * 2),
+          ),
+          TF(
+            controller: firstNameController,
+            onSaved: (val) {
+              userModel.first_name = val;
+            },
+            validateFn: (val) {
+              if (val.isEmpty) return 'Cannot be empty';
+            },
+            label: TEXT(
+              text: 'First Name',
+              fontSize: 16,
+              fontFamily: 'Gilroy-Medium',
+            ),
+            hintText: 'Juliana',
+          ),
+          TF(
+            controller: lastNameController,
+            onSaved: (val) {
+              userModel.last_name = val;
+            },
+            label: TEXT(
+              text: 'Last Name',
+              fontSize: 16,
+              fontFamily: 'Gilroy-Medium',
+            ),
+            validateFn: (val) {
+              if (val.isEmpty) return 'Cannot be empty';
+            },
+            hintText: 'Orji',
+          ),
+          TF(
+            controller: emailController,
+            onSaved: (val) {
+              userModel.email = val;
+            },
+            label: TEXT(
+              text: 'Email',
+              fontSize: 16,
+              fontFamily: 'Gilroy-Medium',
+            ),
+            validateFn: (val) {
+              if (val.isEmpty) return 'Cannot be empty';
+            },
+            hintText: 'Julianamonday@gmail.com',
+          ),
+          TF(
+            controller: phoneController,
+            onSaved: (val) {
+              userModel.phone = val;
+            },
+            label: TEXT(
+              text: 'Phone Number',
+              fontSize: 16,
+              fontFamily: 'Gilroy-Medium',
+            ),
+            validateFn: (val) {
+              if (val.isEmpty) return 'Cannot be empty';
+            },
+            hintText: '09065233507',
+          ),
+          BTN(
+              children: [
+                TEXT(
+                  text: 'Next',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  edgeInset: EdgeInsets.all(0.0),
+                )
+              ],
+              onTap: () {
+                myKey.currentState.save();
+                if (myKey.currentState.validate()) {
+                  setState(() {
+                    _formStage = 2;
+                  });
+                }
+              }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TEXT(text: "Already have an account?"),
+              GestureDetector(
+                child: TEXT(
+                  text: 'Log in',
+                  color: Constants.purple,
+                ),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, 'login');
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildOtp(double height, SignUpVM model) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            TEXT(
+              text: 'OTP Verification',
+              fontFamily: 'Gilroy-bold',
+              fontSize: height * 1.5,
+              edgeInset: EdgeInsets.only(bottom: height * 1.3),
+            ),
+            TEXT(
+              text: 'Enter the OTP sent to ********353',
+              fontWeight: FontWeight.w300,
+              edgeInset: EdgeInsets.only(bottom: height * 1.3),
+            ),
+          ],
+        ),
+        Container(
+            padding: EdgeInsets.only(top: height * 5, bottom: height * 5),
+            child: OTPPin(
+              showFieldAsBox: false,
+              fields: 6,
+            )),
+        Padding(
+          padding: EdgeInsets.only(bottom: height / 1.8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TEXT(
+                text: "Didn't receive the OTP?  ",
+                fontSize: height / 1.5,
+              ),
+              GestureDetector(
+                  child: TEXT(
+                      text: "Resend SMS",
+                      color: Constants.purple,
+                      fontSize: height / 1.5),
+                  onTap: () {}),
+            ],
+          ),
+        ),
+        TEXT(text: "Wrong phone number?", fontSize: height / 1.5),
+        BTN(
+            margin: EdgeInsets.only(top: height * 3),
+            children: [
+              Expanded(
+                  child: TEXT(
+                text: 'Verify',
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                textAlign: TextAlign.center,
+                edgeInset: EdgeInsets.all(0.0),
+              )),
+              SizedBox(
+                  height: 18,
+                  width: 18,
+                  child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                          !model.savingUser ? Constants.purple : Colors.black)))
+            ],
+            onTap: () {
+              model.register(userModel, context);
+            },
+            mainAxisAlignment: model.savingUser
+                ? MainAxisAlignment.end
+                : MainAxisAlignment.center)
+      ],
+    );
+  }
+
+  bool pictureUploaded = false;
+  bool idUploaded = false;
+
+  Widget buildStage(double height, SignUpVM model) {
+    switch (_formStage) {
+      case 1:
+        return personInformation(height);
+        break;
+      case 2:
+        return buildOtp(height, model);
+        break;
+      default:
+        return personInformation(height);
+    }
+  }
+
+  Container _buildCard(
+      IconData icon, String title, String body, double height, bool uploaded) {
+    return Container(
+      padding: EdgeInsets.only(top: 18, bottom: 18),
+      child: ListTile(
+        isThreeLine: true,
+        leading: Icon(icon, color: Constants.purple, size: height * 2),
+        title: TEXT(
+          text: title,
+          fontFamily: 'Gilroy-Medium',
+          fontSize: 16,
+        ),
+        trailing: uploaded
+            ? Container(
+                child: Icon(Icons.check, color: Colors.white),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(style: BorderStyle.none),
+                    color: Constants.purple),
+              )
+            : Text(''),
+        subtitle: TEXT(
+            text: body, fontSize: 12, color: Color.fromRGBO(85, 85, 85, 1)),
+      ),
+      margin: EdgeInsets.only(bottom: height),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border(
+              bottom: BorderSide.none,
+              top: BorderSide.none,
+              left: BorderSide.none,
+              right: BorderSide.none),
+          boxShadow: [
+            BoxShadow(
+                color: Color.fromRGBO(174, 174, 192, 1),
+                spreadRadius: 0.4,
+                blurRadius: 20)
+          ]),
+    );
+  }
+
+  TextEditingController firstNameController =
+      new TextEditingController(text: '');
+  TextEditingController lastNameController =
+      new TextEditingController(text: '');
+  TextEditingController emailController = new TextEditingController(text: '');
+  TextEditingController phoneController = new TextEditingController(text: '');
+  TextEditingController passwordController =
+      new TextEditingController(text: '');
+
+  BeneficiaryUserModel userModel = new BeneficiaryUserModel();
+
+  final myKey = GlobalKey<FormState>();
+  int _formStage = 1;
+}
---
+++ lib/screens/ui/viewModels/sign_upVM.dart
@@ -0,0 +1,150 @@
+import 'dart:convert';
+// import 'dart:io';
+import 'package:CHATS/ChatsMain/core/models/beneficiary_user_model.dart';
+import 'package:CHATS/providers/base_provider_model.dart';
+import 'package:CHATS/ChatsMain/core/services/authentication_service.dart';
+import 'package:CHATS/services/local_storage_service.dart';
+import 'package:CHATS/domain/locator.dart';
+import 'package:CHATS/ChatsMain/ui/shared/ui_helper.dart';
+import 'package:flutter/widgets.dart';
+
+class SignUpVM extends BaseProviderModel {
+  List<String> imageList = [
+    'assets/onboard2.jpeg',
+    'assets/onboard3.jpeg',
+    'assets/onboard1.jpeg'
+  ];
+
+  List<Widget> scrollText = [
+    Container(
+      height: 400,
+      child: Column(
+        children: [
+          Container(
+            height: 20,
+            child: Text(
+              'Simple  And Easy',
+              style: Constants.kboldTextStyle,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              height: 20,
+              child: Text('Easy and Straightforward onboarding\n process'))
+        ],
+      ),
+    ),
+    Container(
+      height: 400,
+      child: Column(
+        children: [
+          Container(
+            height: 20,
+            child: Text(
+              'Safe  And Secure',
+              style: Constants.kboldTextStyle,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 50,
+            child: Text(
+                'Convexity is safe  and secured to set up \n \t encrypted security using blockchain'),
+          )
+        ],
+      ),
+    ),
+    Container(
+      height: 400,
+      child: Column(
+        children: [
+          Container(
+            height: 20,
+            child: Text(
+              'Pay Money Instantly',
+              style: Constants.kboldTextStyle,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              height: 50,
+              child: Text(
+                  'Transfer instantly between vendors and \n \t\t\t                   beneficiaries'))
+        ],
+      ),
+    ),
+  ];
+
+  final keyStore = locator<SharedPref>();
+
+  Future startUp(BuildContext context) async {
+    if (await keyStore.myFirst ?? true) {
+      print(keyStore.myFirst);
+      await Future.delayed(Duration(seconds: 4));
+      keyStore.setIsFirstTime(false);
+      Navigator.pushNamed(context, 'onboard');
+    } else {
+      await Future.delayed(Duration(seconds: 4));
+      Navigator.pushNamed(context, 'login');
+      // await Future.delayed(Duration(seconds: 2));
+      // Navigator.pushNamed(context, home);
+    }
+  }
+
+  login(String email, String password, BuildContext context) async {
+    if (isBusy) return;
+    errorMessage = '';
+    isBusy = true;
+    notifyListeners();
+    await _authenticationService.login(email, password).then((value) {
+      // Sends user to homepage if credentials are correct
+      if (value['code'] == 200) {
+        Navigator.pushReplacementNamed(context, '/');
+      } else {
+        errorMessage = value['message'];
+        print(value['code']);
+      }
+    }).catchError((e) {
+      print(e);
+    });
+    isBusy = false;
+    notifyListeners();
+  }
+
+  register(BeneficiaryUserModel model, BuildContext context) async {
+    // model.profile_pic = profilePicture;
+    print(jsonEncode(model.toJson()));
+    savingUser = true;
+    signUpErrorMessage = '';
+    notifyListeners();
+    await _authenticationService.register(model).then((value) {
+      if (value['code'] == 201) {
+        Navigator.pushReplacementNamed(context, '/');
+      } else {
+        signUpErrorMessage = value['message'];
+      }
+    }).catchError((e) {
+      print(e);
+      print("ss");
+    });
+    savingUser = false;
+    notifyListeners();
+  }
+
+  bool isBusy = false;
+  bool savingUser = false;
+  String errorMessage = '';
+  String signUpErrorMessage = '';
+
+  AuthenticationService _authenticationService = new AuthenticationService();
+
+  String profilePicture;
+
+  String validId;
+}
