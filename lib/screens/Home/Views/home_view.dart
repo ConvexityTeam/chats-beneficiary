6cac9b33b936f4f89e78d1b152336e359200953f
ae-allison <ansonervin@yahoo.com>
2021-04-05 07:57:55 -0500

    Removed previous transaction list from home

--- lib/screens/Home/Views/home_view.dart
+++ lib/screens/Home/Views/home_view.dart
@@ -1,5 +1,7 @@
+import 'package:CHATS/api/transactions.dart';
 import 'package:CHATS/screens/home/Views/drawer_view.dart';
 import 'package:CHATS/utils/colors.dart';
+import 'package:CHATS/widgets/home/recent_transaction/recent_transaction_list.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -145,62 +147,10 @@ class HomeView extends StatelessWidget {
                       SizedBox(width: 10),
                     ],
                   ),
-                  Column(
-                      children: List.generate(
-                    10,
-                    (index) => buildDetails(),
-                  ))
+                  RecentTransactionsList()
                 ],
               ),
             ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildDetails() {
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: ListTile(
-        leading: Container(
-          decoration: BoxDecoration(
-              color: Color.fromRGBO(240, 240, 240, 0.4),
-              borderRadius: BorderRadius.all(
-                Radius.circular(5),
-              )),
-          height: 62,
-          width: 62,
-          child: Center(
-            child: Text(
-              "C",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontFamily: "Gilroy-medium",
-              ),
-            ),
-          ),
-        ),
-        title: Text(
-          "Dangote Nigeria",
-          style: TextStyle(
-            fontFamily: "Gilroy-medium",
-          ),
-        ),
-        trailing: Text(
-          "-\$${1},500.75",
-          style: TextStyle(
-            color: Color(0xffc20000),
-            fontFamily: "Gilroy-medium",
-          ),
-        ),
-        subtitle: Text(
-          "25 Nov 2020",
-          style: TextStyle(
-            color: Color(0xff333333),
-            fontSize: 13,
-            fontFamily: "Gilroy-medium",
           ),
         ),
       ),
